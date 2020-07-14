dd0534259cc18a0f719474d7a925a92ffc8edec3
Claudia Quispe <claudiaquispe@MacBook-Pro-de-Claudia.local>
2020-07-14 10:53:02 -0400

    proyecto con error en localhost

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div id="app">
+  <div id="Lista">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Lista titulo="Blood Orange" v-bind:enlaces="BloodOrange" />
+    <Lista titulo="Cuco" v-bind:enlaces="Cuco" /> 
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Lista from './components/Lista.vue'
 
 export default {
-  name: 'App',
+  name: 'Lista',
   components: {
-    HelloWorld
+    Lista
+  },
+  data:() => {
+    return {
+      //aca estan mis datos
+      BloodOrange: [ //aca va mi dato interno
+        {texto: 'Coastal Grooves',url: 'https://www.youtube.com/watch?v=uXLEIlQJX6A'},
+        {texto: 'Cupid Deluxe', url:'https://www.youtube.com/watch?v=RBHwO-2Amzs&list=PLMNMmvIC2uGaQVq0rLqHWrSVtxdyO9fYT'},
+        {texto: 'Freetown Sound', url:'https://www.youtube.com/watch?v=EK6R6WfKo0M&list=PLxKHVMqMZqUS7chrq-OLeE4rX9hYyml5B'},
+        {texto: 'Negro Swan', url:'https://www.youtube.com/watch?v=3fSgsvJC9Nw&list=PLISgjPzhNbvZQXXY7qH1aG8YDf0MA27z2'},
+        {texto: 'Angel`s Pulse', url:'https://www.youtube.com/watch?v=lBFdqb3P1lA&list=PLg1UyK6Rptxsan7iGWqld2Ki8Wik_PHz9'},
+      ],
+      Cuco: [
+        {texto: 'Para Mi',url: 'https://www.youtube.com/watch?v=2qrX7n5O9m0&list=PLFJ4rD4UhAdpE1YGPME_9caqb2en6BYis'},
+        {texto: 'Songs4u',url: 'https://www.youtube.com/watch?v=n-4gqm6Ah-A&list=PLmd3unaE1YCLFcQnZ6d_JJprXWChaQ0A3'},
+        {texto: 'Wannabewithu',url: 'https://www.youtube.com/watch?v=9wiEM0s4aCQ&list=PLmd3unaE1YCJCplCwB04ji7SmjAgPI4QZ'},
+        {texto: 'A24',url: 'https://www.youtube.com/watch?v=4iB8VUSMfjE'},
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Lista.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="lista">
+      <h3> {{ titulo }} </h3>
+      <ol>
+          <li v-for="(lista1, i) in enlaces" v-bind:key="i">{{ lista1 }}</li>
+      </ol>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Lista',
+  props: {
+    titulo: String,
+    enlaces: Array
+  }
+}
+
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 30px;
+  background-color:cadetblue;
+}
+
+</style>
